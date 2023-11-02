3358bb8fa0b3d551f3e0145039ac1860397ee690
cande <candelariamanciniarino@gmail.com>
2023-11-02 11:56:34 -0300

    en camino

--- client/src/Views/detailPage/detailPage.jsx
+++ client/src/Views/detailPage/detailPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector, useParams } from 'react-redux'
+import {getById} from '../../Redux/actions/actions';
+import Card from '../../Components/Card/Card';
 
-const Details = () => {
+
+
+
+function Details () {
+  const params = useParams()
+  const dispatch = useDispatch()
+
+  const game =useSelector((state) => state.gameId)
+  const [serchId, setSearchId]= useState('')
+
+  function handleChange(event){
+    event.preventDefault()
+    setSearchId(event.target.value)
+  }
+
+
+  function handleSubmit(event){
+    event.preventDefault();
+    dispatch(getById(serchId));
+  }
+
+
+useEffect(()=>{
+  dispatch(getById())
+},[dispatch]);
+
+
   return (
-    <div>Details</div>
-  )
+    <div> {game?.map(game=>{
+
+      <Card
+       id={game.id}
+       name={game.name}
+       background_image={game.background_image}
+       plataforms={game.plataforms}
+       releasseData={game.releasseData}
+       description={game.description}
+       rating={game.rating}
+       genres={game.genres}
+       handleSubmit={game.handleSubmit}
+       handleChange={game.handleChange} />
+
+    })}
+
+    <p>MiDetails</p>
+    
+    </div>
+  );
 }
 
-export default Details
+export default Details;
